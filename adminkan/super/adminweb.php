7564337bc26f85fe95ab1d878e58e4333aa8751d
DelphicGen <gennardokidam@gmail.com>
2021-07-17 19:56:21 +0700

    make dashboard table responsive

--- adminkan/super/adminweb.php
+++ adminkan/super/adminweb.php
@@ -37,7 +37,8 @@ while($tampilQuery = mysqli_fetch_array($queryLihat)){
   <link rel="stylesheet" href="asset/bower_components/bootstrap-daterangepicker/daterangepicker.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="asset/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="css/style.css">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
