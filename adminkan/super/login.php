6375fc7ea0d08261334525fcc5e2cc7c8c5ec54a
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2021-07-19 08:37:23 +0700

    url super Redirect role akun biasa ke url tanpa super

--- adminkan/super/login.php
+++ adminkan/super/login.php
@@ -4,8 +4,8 @@ $username = $_POST ['username'];
 $password = $_POST ['password'];
 if (!ctype_alnum($username) OR !ctype_alnum($password)) {
 	echo "<center>LOGIN GAGAL! <br>
-	Username atau password anda tidak benar.<br>
-	Atau akun anda sedang diblokir.<br>";
+	Username atau password Anda tidak benar.<br>
+	Atau akun Anda sedang diblokir.<br>";
 	echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
 } else {
 	$login = mysqli_query($konek, "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'");
@@ -17,13 +17,29 @@ if (!ctype_alnum($username) OR !ctype_alnum($password)) {
 		$_SESSION['namauser'] = $r['username'];
 		$_SESSION['passuser'] = $r['password'];
 		$_SESSION['id_admin'] = $r['id_admin'];
+		$_SESSION['nama']=$r['nama'];
+    	$_SESSION['level_user']=$r['level_user'];
+		
+		$level_user=$r['level_user'];
 
-		header('location:adminweb.php?module=home');
+		if($level_user=='Admin Super'){
+            header('location:super/adminweb.php?module=home');
+        }
+		elseif($level_user=='Admin Biasa'){
+			header('location:adminweb.php?module=home');
+		}
+		else{
+			echo "<center>LOGIN GAGAL! <br>
+			Username atau password Anda tidak benar.<br>
+			Atau akun Anda sedang diblokir.<br>";
+			echo "<a href=index.php><b>Mohon Coba Login Kembali</b></a></center>";
+		}
+
 	} else {
 		echo "<center>LOGIN GAGAL! <br>
-		Username atau password anda tidak benar.<br>
-		Atau akun anda sedang diblokir.<br>";
-		echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+		Username atau password Anda tidak benar.<br>
+		Atau akun Anda sedang diblokir.<br>";
+		echo "<a href=index.php><b>Mohon Coba Login Kembali</b></a></center>";
 	}
 }
  ?>
