dff6ffb43d8fe6b671a42b27d78b102ac202e111
DelphicGen <gennardokidam@gmail.com>
2021-07-19 16:49:16 +0700

    index.php issue

--- index.php
+++ index.php
@@ -75,8 +75,7 @@
 											<li><a href="paket-wisata.php">paket wisata</a></li>
 											<li><a href="blog.php">blog</a></li>
 											<li><a href="tentang-kami.php">tentang kami</a></li>
-											<li><a href="#">downloads</a></li>
-											<li><a href="kontak.php" class="clv_btn2">kontak kami</a></li>
+											<li><a href="kontak.php" class="clv_btn2">kontak</a></li>
 										</ul>
 									</div>
 									<div class="cart_nav">
@@ -312,11 +311,10 @@
 							<br>
 							<div class="row justify-content-center">
 								<p>Produk kami juga tersedia di </p>
-								
 							</div>
 							<center>
 								<a href="https://shopee.co.id/desa_ngargoretno" target="_blank" rel="noopener noreferrer"><img src="adminkan/img/image 18.png" alt="logo_shopee" style="width: 13%; object-fit: contain;"></a> &emsp; &emsp; 
-								<a href="https://www.tokopedia.com/desangargoretno" target="_blank" rel="noopener noreferrer"><img src="adminkan/img/image 19.png" alt="logo_tokopedia" style="width: 14%; object-fit: contain;"></a>
+								<a href="https://www.tokopedia.com/desangargoretno" target="_blank" rel="noopener noreferrer"><img src="adminkan/img/image 19.png" alt="logo_tokopedia" style="width: 13%; object-fit: contain;"></a>
 							</center>
 						</div>
 					</div>
@@ -453,11 +451,6 @@
 								<?php } ?>
 							</div>
 						</div>
-					</div>
-					<div class="row justify-content-center">
-						<a href="blog.php" class="clv_btn">Lihat Semua Artikel
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg>
-						</a>
 					</div>
 				</div>
 			</div>
