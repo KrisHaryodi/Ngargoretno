228cd0f90773009d5df65b2bda4a632456274a0e
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2021-07-19 15:35:26 +0700

    Add button lihat smua artikel

--- index.php
+++ index.php
@@ -451,6 +451,11 @@
 							</div>
 						</div>
 					</div>
+					<div class="row justify-content-center">
+						<a href="blog.php" class="clv_btn">Lihat Semua Artikel
+							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
